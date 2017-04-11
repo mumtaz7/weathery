90d2ca213f7bab416a1ce0aaf21cc08b0e17be17
Mumtaz Khan <mumtzkhan7@gmail.com>
2017-04-11 23:26:56 +0000

    fixed top nav, added create action

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,5 +7,15 @@ class CitiesController < ApplicationController
     @city = City.new
   end
 
+  def create
+    City.create(city_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def city_params
+    params.require(:city).permit(:temperature, :conditions)
+  end
 
 end
